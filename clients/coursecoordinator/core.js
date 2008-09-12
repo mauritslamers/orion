16c61d9404c0257d0ddc6d7ed6d30fc638c47088
Maurits Lamers <maurits@weidestraat.nl>
2008-09-13 01:30:12 +0200

    Broken. Starting to implement list_view instead of collection_view and source_list_view 2

--- clients/coursecoordinator/core.js
+++ clients/coursecoordinator/core.js
@@ -11,15 +11,7 @@ CourseCoordinator = SC.Object.create({
   // When you are in development mode, this array will be populated with
   // any fixtures you create for testing and loaded automatically in your
   // main method.  When in production, this will be an empty array.
-  FIXTURES: [],
-  
-  start: function(){
-    OrionFw.server.preload(OrionFw.FIXTURES); 
-    var courses = OrionFw.Education.collection();
-    courses.set('orderBy',['name DESC']);
-    courses.refresh();
-    CourseCoordinator.CM_courseListController.set('content',courses);
-  }
+  FIXTURES: []
 
 }) ;
 
