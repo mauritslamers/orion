bd4d04907f4955887e8ee843f5b20c40292a4562
Maurits Lamers <maurits@weidestraat.nl>
2008-09-22 14:46:29 +0200

    Adding controllers for add_subjects_to_course_pane

---
+++ clients/coursecoordinator/controllers/cm_add_subjects_to_course_pane_available_departments.js
@@ -0,0 +1,21 @@
+// ==========================================================================
+// Coursecoordinator.CmAddSubjectsToCoursePaneAvailableDepartmentsController
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document Your View Here)
+
+  @extends SC.Object
+  @author AuthorName
+  @version 0.1
+  @static
+*/
+CourseCoordinator.CM_addSubjectsToCoursePaneAvailableDepartmentsController = SC.Object.create(
+/** @scope Coursecoordinator.cmAddSubjectsToCoursePaneAvailableDepartmentsController */ {
+
+  // TODO: Add your own code here.
+
+}) ;
