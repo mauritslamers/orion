efe8c3e96f81a664fe41aa0cc4a0d88027e7d35a
Maurits Lamers <maurits@Computer-van-Maurits-Lamers.local>
2008-09-22 21:45:00 +0200

    Added admission exam client

---
+++ clients/admissionexam/controllers/candidate_choice.js
@@ -0,0 +1,22 @@
+// ==========================================================================
+// Admissionexam.CandidateChoiceController
+// ==========================================================================
+
+require('core');
+require('orion_fw');
+
+/** @class
+
+  (Document Your View Here)
+
+  @extends SC.Object
+  @author AuthorName
+  @version 0.1
+  @static
+*/
+AdmissionExam.candidateChoiceController = SC.CollectionController.create(
+/** @scope Admissionexam.candidateChoiceController */ {
+
+  // TODO: Add your own code here.
+
+}) ;
